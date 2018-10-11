9389abda63b9661b20a5d88e455269a3b3ad271a
Irfan <irfankk1993@gmail.com>
2018-10-11 08:45:21 +0530

    change

--- data-analysis.py
+++ data-analysis.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.plotly as py
 
 data = pd.read_csv("test.csv") 
 
 # Speed controlled road
 controled_access = data[data.controlledAccess != 'N']
-
-+
+py.iplot(controled_access, filename='controled_access')
 
 # roads has no speed control
 not_controled_access = data[data.controlledAccess != 'Y']
+py.iplot(not_controled_access, filename='not_controled_access')
 
 # Highway list
 highway = data[data.isHighway != '0']
+py.iplot(highway, filename='highway')
 
 #Local roads
 local_road = data[data.isHighway != '0']
-
+py.iplot(local_road, filename='local_road')
 
 #
 max_speed = df[['latitude', 'longitude', 'speedlimit_mph', 'driver_id']]
@@ -26,4 +28,5 @@ vehicle_speed['speed'] = max_speed.speed[max_speed.speedlimit_mph < vehicle_spee
 vehicle_speed = vehicle_speed[vehicle_speed.speed != False]
 # Best drivers data frame
 vehicle_speed = vehicle_speed[vehicle_speed.speed_mph < 10]
+py.iplot(vehicle_speed, filename='vehicle_speed')
 
